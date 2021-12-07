9c9bf95618085a576fc77b3e81d9c849bec25e03
esspee39 <shadowphoenix39@gmail.com>
2021-12-07 11:35:18 -0500

    Add new function to example.cpp

--- src/example.cpp
+++ src/example.cpp
@@ -5,3 +5,5 @@ double add_numbers(const double f1, const double f2) { return f1 + f2; }
 double subtract_numbers(const double f1, const double f2) { return f1 - f2; }
 
 double multiply_numbers(const double f1, const double f2) { return f1 * f2; }
+
+bool check_number_equality(const double f1, const double f2) { return f1 == f2; }
